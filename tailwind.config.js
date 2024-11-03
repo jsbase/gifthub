2374e1a0a392c4f79e8a4b610f8dcfadd6a4d019
jsbase <andre.siedersleben@gmail.com>
2024-11-03 13:48:01 +0100

    Change tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   theme: {
-    extend: {},
+    extend: {
+      borderColor: {
+        border: 'hsl(var(--border))',
+      },
+      backgroundColor: {
+        background: 'hsl(var(--background))',
+      },
+      textColor: {
+        foreground: 'hsl(var(--foreground))',
+      },
+    },
   },
   plugins: [],
 }
-
