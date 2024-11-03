ff3e1732312c4dea00ae5cad3592574cfd3d6405
jsbase <andre.siedersleben@gmail.com>
2024-11-03 01:56:05 +0100

    Change prisma

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import { SignJWT } from 'jose';
 import bcrypt from 'bcryptjs';
-import { prisma } from '@/lib/prisma';
+import prisma from '@/lib/prisma';
 
 export async function POST(request: Request) {
   try {
