34909db92f760f61a9bccb7aadb20c1a7ffa9f2b
jsbase <andre.siedersleben@gmail.com>
2024-11-05 15:55:48 +0100

    Fix favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,11 +27,20 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
+      { url: '/favicon.ico' },
       { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
       { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/android-chrome-192x192.png', sizes: '192x192', type: 'image/png' },
+      { url: '/android-chrome-512x512.png', sizes: '512x512', type: 'image/png' },
     ],
     apple: [
-      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
+      { url: '/apple-touch-icon.png' },
+    ],
+    other: [
+      {
+        rel: 'manifest',
+        url: '/site.webmanifest',
+      },
     ],
   },
   manifest: '/site.webmanifest',
@@ -50,10 +59,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-        <meta name="viewport" content={`width=${viewport.width}, initial-scale=${viewport.initialScale}`} />
-      </head>
-
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
