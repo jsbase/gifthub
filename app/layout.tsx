894e0ede19295f254260d7a78bb10ee4c25841e0
jsbase <andre.siedersleben@gmail.com>
2024-11-05 19:45:16 +0100

    Development (#25)
    * Change better viewports
    
    * Change modal dialogs
    
    * Change better add gift form
    
    * Update header.tsx
    
    * Add service-worker
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,11 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { Toaster } from "@/components/ui/sonner";
 import { ThemeProvider } from "@/components/theme-provider";
+import { registerServiceWorker } from './sw';
+
+if (typeof window !== 'undefined') {
+  registerServiceWorker();
+}
 
 const inter = Inter({ subsets: ['latin'] });
 
