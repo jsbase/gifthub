b43ff5918b51c60238e284bba95a09d5fb4adf31
jsbase <isbase.dev>
2024-11-07 23:28:23 +0100

    Change dashboard.tsx

--- components/feature-card.tsx
+++ components/feature-card.tsx
@@ -2,7 +2,7 @@ import { FeatureCardProps } from "@/types";
 
 export function FeatureCard({ title, description }: FeatureCardProps) {
   return (
-    <div className="block p-6 bg-background border rounded-lg shadow dark:bg-card-background dark:border-gray-700">
+    <div className="block mb-4 p-6 bg-background border rounded-lg shadow dark:bg-card-background dark:border-gray-700">
       <h5 className="mb-2 text-2xl font-bold tracking-tight text-foreground">
         {title}
       </h5>
